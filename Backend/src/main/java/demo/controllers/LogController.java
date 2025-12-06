3b3ff8304ffb7d66ce3e538eef1b9ad542a22b0f
sardq <dreod@mail.ru>
2025-12-06 14:17:44 +0400

    completed task 4 without tests

---
+++ Backend/src/main/java/demo/controllers/LogController.java
@@ -0,0 +1,48 @@
+package demo.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import demo.services.LogService;
+
+import java.io.InputStream;
+
+@RestController
+@RequestMapping("/api/log")
+public class LogController {
+
+    private final LogService logService;
+    private static final Logger logger = LoggerFactory.getLogger(LogController.class);
+
+    public LogController(LogService logService) {
+        this.logService = logService;
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<InputStreamResource> uploadThenDownloadLog() {
+        logger.info("Запрос на загрузку лога");
+        try {
+            String filePath = "logs/application.log";
+            String objectName = "application.log";
+
+            logService.uploadLogFileToMinio(filePath, objectName);
+
+            InputStream inputStream = logService.downloadLogFileFromMinio(objectName);
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + objectName + "\"")
+                    .contentType(MediaType.TEXT_PLAIN)
+                    .body(new InputStreamResource(inputStream));
+
+        } catch (Exception e) {
+
+            logger.error("Ошибка при загрузке лога");
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+}
---
+++ Backend/src/main/java/demo/services/LogService.java
@@ -0,0 +1,53 @@
+package demo.services;
+
+import io.minio.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Service
+public class LogService {
+
+    private final MinioClient minioClient;
+    private static final Logger logger = LoggerFactory.getLogger(LogService.class);
+
+    @Value("${minio.bucket}")
+    private String bucketName;
+
+    public LogService(
+            @Value("${minio.url}") String url,
+            @Value("${minio.access-key}") String accessKey,
+            @Value("${minio.secret-key}") String secretKey) {
+        this.minioClient = MinioClient.builder()
+                .endpoint(url)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+    public void uploadLogFileToMinio(String filePath, String objectName) throws Exception {
+        logger.info("Попытка загрузить лог на сервер:{}", filePath, objectName);
+
+        InputStream is = Files.newInputStream(Paths.get(filePath));
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(objectName)
+                        .stream(is, is.available(), -1)
+                        .contentType("text/plain")
+                        .build());
+    }
+
+    public InputStream downloadLogFileFromMinio(String objectName) throws Exception {
+        logger.info("Попытка загрузить лог на устройство:{}", objectName);
+        return minioClient.getObject(
+                GetObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(objectName)
+                        .build());
+    }
+}
